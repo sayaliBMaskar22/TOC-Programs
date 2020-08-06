e3fd4d3aee6bbd494f49ff114e81aa59f7ba9a20
Sayali B Maskar <64319753+sayaliBMaskar22@users.noreply.github.com>
2020-08-06 20:05:02 +0530

    Initial commit

---
+++ Divisibilityby3Decimal.py
@@ -0,0 +1,11 @@
+q0=0
+q1=1
+q2=2
+num=int(input("Enter the number"))
+if num%3==0:
+    print(num,"is divisible by 3","\n Reminder is:",q0)
+elif num%3==1:
+    print(num,"is divible by 3","\n Reminder is:",q1)
+elif num%3==2:
+    print(num,"is divisible by 3","\n Reminder is:",q2)
+print("Program Executed!!")
